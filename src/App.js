e7bc0634a494839a1f7c8af693b3c9bc6d3e3975
aritradey-CS <81703791+aritradey-CS@users.noreply.github.com>
2023-05-13 18:31:01 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,81 @@
+import { useState } from "react";
+
+function App() {
+  const slots = useState(Array.from(new Array(100), (v, i) => i))[0];
+  const [active, setActive] = useState(0);
+
+  const getCurrentRow = () => {
+    if (active >= 0 && active <= 9) return 1;
+    if (active >= 10 && active <= 19) return 2;
+    if (active >= 20 && active <= 29) return 3;
+    if (active >= 30 && active <= 39) return 4;
+    if (active >= 40 && active <= 49) return 5;
+    if (active >= 50 && active <= 59) return 6;
+    if (active >= 60 && active <= 69) return 7;
+    if (active >= 70 && active <= 79) return 8;
+    if (active >= 80 && active <= 89) return 9;
+    if (active >= 90 && active <= 99) return 10;
+    return 0;
+  };
+
+  const leftHandler = () => {
+    const currentRow = getCurrentRow(); // 1
+    const minPos = currentRow * 10 - 10; // 1*10 -10 = 0 -> min
+    if (active === minPos) return;
+    setActive((prev) => prev - 1);
+  };
+
+  const rightHandler = () => {
+    const currentRow = getCurrentRow(); // 1
+    const maxPos = currentRow * 10 - 1; // 1*10 -1 = 9 -> max
+    if (active === maxPos) return;
+    setActive((prev) => prev + 1);
+  };
+
+  const downHandler = () => {
+    const currentRow = getCurrentRow();
+    if (currentRow === 10) return;
+    setActive((prev) => prev + 10);
+  };
+
+  const upHandler = () => {
+    const currentRow = getCurrentRow();
+    if (currentRow === 1) return;
+    setActive((prev) => prev - 10);
+  };
+
+  // useEffect(() => {
+  //   const handler = (e: any) => {
+  //     if (e.which === 39) rightHandler();
+  //     if (e.which === 37) leftHandler();
+  //     if (e.which === 38) upHandler();
+  //     if (e.which === 40) downHandler();
+  //   };
+  // //   window.addEventListener("keydown", handler);
+
+  // //   return () => {
+  // //     window.removeEventListener("keydown", handler);
+  // //   };
+  // // }, [active]);
+
+  return (
+    <main>
+      <div>
+        <div className="area">
+          {slots.map((sl) => (
+            <div
+              className={`box ${sl === active ? "active" : ""}`}
+              key={sl}
+            ></div>
+          ))}
+        </div>
+        <button className="button-up" onClick={upHandler}>UP</button>
+        <button onClick={downHandler}>Down</button>
+        <button onClick={leftHandler}>Left</button>
+        <button onClick={rightHandler}>Right</button>
+      </div>
+    </main>
+  );
+}
+
+export default App;
